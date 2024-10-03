32b06d498ea4837e1aa08ed37cc8683a05ed37dd
Johannes Håkonsen <johannes@tibe.no>
2024-10-03 15:19:27 +0200

    feat: added button component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ComputedRef } from 'vue';
+
+import Button from './components/Button.vue'
+
+function handleButtonClick() {
+    alert('Clicked button');
+}
+
+const cssVars = computed(() => {
+    return {
+        '--primary-color'     : '#007bff',
+        '--secondary-color'   : '#2e6e49',
+        '--border-hover-color': '#646cff'
+    }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="buttons">
+        <Button 
+            :style="cssVars"
+            label="Test Button" 
+            icon="./assets/star.svg" 
+            iconPosition="before" 
+            variant="primary" 
+            @click="() => handleButtonClick()"></Button>
+
+        <Button 
+            :style="cssVars"
+            label="No icon button"
+            variant="secondary"></Button>
+    
+        <Button 
+            :style="cssVars"
+            label="Hyperlink button" 
+            icon="./assets/arrow.svg" 
+            iconPosition="after" 
+            href="https://www.google.com"></Button>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.buttons {
+    display: flex;
+    gap: 1rem;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+defineProps({
+    label: String,
+    icon: String,
+    iconPosition: {
+        type: String as PropType<'before' | 'after'>,
+        default: 'before'
+    },
+    href: String,
+    variant: {
+        type: String as PropType<'primary' | 'secondary'>,
+        default: 'primary'
+    },
+    onClick: Function as PropType<() => void>
+});
+
+</script>
+
+<template>
+    <component 
+        :is="href ? 'a' : 'button'" 
+        :href="href" 
+        :target="href ? '_blank' : null"
+        :rel="href ? 'noopener noreferrer' : null"
+        @click="$emit('click')"
+        :class="['button', `button--${variant}`, { 'button--has-icon': icon }]">
+
+        <img v-if="icon && iconPosition === 'before'" class="button__icon button__icon--before" :src="icon" alt="">
+        <span class="button__label" v-if="label">{{ label }}</span>
+        <img v-if="icon && iconPosition === 'after'" class="button__icon button__icon--after" :src="icon" alt="">
+    </component>
+</template>
+
+<style scoped>
+    .button {
+        display: inline-flex;
+        align-items: center;
+        cursor: pointer;
+        border: 1px solid transparent;
+        padding: 0.5rem;
+        border-radius: 0.25rem;
+        font-size: 1rem;
+        font-weight: 500;
+        font-family: inherit;
+        cursor: pointer;
+        transition: border-color 0.25s;
+    }
+    .button:hover,
+    .button:focus,
+    .button:focus-visible {
+        outline: 4px auto -webkit-focus-ring-color;
+    }
+    .button--primary {
+        background-color: var(--primary-color);
+        color: white;
+    }
+    .button--secondary {
+        background-color: var(--secondary-color);
+        color: white;
+    }
+    .button__icon {
+        width: 1.5rem;
+        height: 1.5rem;
+    }
+    .button__icon--before {
+        margin-right: 1rem;
+    }
+    .button__icon--after {
+        margin-left: 1rem;
+    }
+</style>
